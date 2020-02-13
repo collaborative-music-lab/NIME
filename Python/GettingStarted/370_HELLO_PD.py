3ac4ec4bdf28ce79c49fc3f4dba04ee89e0f1e4a
Ian Hattwick <ian@ianhattwick.com>
2020-02-12 19:19:25 -0500

    small changes

--- Python/GettingStarted/370_HELLO_PD.py
+++ Python/GettingStarted/370_HELLO_PD.py
@@ -34,7 +34,7 @@ ports = list(serial.tools.list_ports.comports())
 for x in range(len(ports)): 
     print (ports[x])
 
-ser = serial.Serial("/dev/cu.usbserial-14440")
+ser = serial.Serial("/dev/cu.usbserial-14310")
 ser.baudrate=115200
 ser.read(ser.in_waiting) # if anything in input buffer, discard it
 
