b16cc00be9acdd3909266ebf40cb9a00d5a9382a
Ian Hattwick <ian@ianhattwick.com>
2022-02-15 00:30:27 -0500

    lab3_test

--- Class/Labs/lab 3/lab3_test/lab3_test.ino
+++ Class/Labs/lab 3/lab3_test/lab3_test.ino
@@ -1,10 +1,9 @@
-
-
- // array of ESPpins
+// array of ESPpins
  //you can just use the first 6 for our analog inputs for now
  //and 14 for both analog and digital inputs
-const byte espPin[] = {32,36,39,35,33,34, //analog inputs
-  13,14,25,26,27,15,0,4, //digital inputs
+const byte espPin[] = 
+  {32,36,39,35,33,34, //analog inputs 0-5
+  13,14,25,26,27,15,0,4, //digital inputs 6-13
   19,23,18,2,12, //SPI
   21,22, //I2C
   16,17, //UART 2
@@ -14,7 +13,15 @@ const byte espPin[] = {32,36,39,35,33,34, //analog inputs
   void setup() {
   Serial.begin(115200);
 
-  for(int i=0;i<  14;i++) pinMode(espPin[i], INPUT);
+  //set first 6 pins to be inputs
+  //this is the analog pins on the PCB
+  for(int i=0;i< 6; i++) pinMode(espPin[i], INPUT);
+
+  //set pins 6-13 to be inputs
+  //these are the digital pins on the PCB
+  //they are pulled up on the PCB so the buttons can just be connected
+  //to ground
+  for(int i=6;i< 23; i++) pinMode(espPin[i], INPUT_PULLUP);
   
   Serial.println("setup complete");
 
@@ -22,11 +29,21 @@ const byte espPin[] = {32,36,39,35,33,34, //analog inputs
 
 
 void loop() {
+  //this is a placeholder for just those pins we are actually using
   byte myPins[] = {0,1,6,7,8,9,10,11,12,13};
+  
   for( int i=0;i<6;i++){
   Serial.print( analogRead( espPin[i]) );
    Serial.print("\t");
   }
   Serial.println();
+  for( int i=6;i<23;i++){
+  Serial.print( digitalRead( espPin[i]) );
+   Serial.print("\t");
+  }
+  Serial.println();
+  Serial.println();
+  
   delay(500);
+  
 }
